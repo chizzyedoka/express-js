31d22664dffc7400ca9778b89f2fd9680c613414
CHISOM EDOKA <edokachisom48@gmail.com>
2023-06-30 13:38:07 +0100

    put requests

--- index.js
+++ index.js
@@ -3,27 +3,38 @@ const express = require("express");
 const app = express();
 const PORT = 3001;
 
+app.use(express.json()); // middlware to post json
+app.use(express.urlencoded());
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
+
+const groceryList = [
+  {
+    item: "milk",
+    quantity: 2,
+  },
+  {
+    item: "cereal",
+    quantity: 1,
+  },
+  {
+    item: "sugar",
+    quantity: 3,
+  },
+];
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Home page");
 });
 
 app.get("/groceries", (req, res) => {
-  res.send([
-    {
-      item: "milk",
-      quantity: 2,
-    },
-    {
-      item: "cereal",
-      quantity: 1,
-    },
-    {
-      item: "sugar",
-      quantity: 3,
-    },
-  ]);
+  res.send(groceryList);
+});
+
+app.post("/groceries", (req, res) => {
+  console.log(req.body);
+  groceryList.push(req.body);
+  res.sendStatus(200);
 });
