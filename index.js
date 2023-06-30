e8cb815cbe8fc0d7d0f68f97956c5b4974dcdf5f
CHISOM EDOKA <edokachisom48@gmail.com>
2023-06-30 14:01:50 +0100

    middlewares

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const app = express();
 const PORT = 3001;
 
 app.use(express.json()); // middlware to post json
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log(req.url);
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
@@ -25,9 +29,17 @@ const groceryList = [
   },
 ];
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the Home page");
-});
+app.get(
+  "/",
+  // adding middleware
+  (req, res, next) => {
+    console.log("Before handling request");
+    next(); // to transfer functionality to the next middleware
+  },
+  (req, res) => {
+    res.send("Welcome to the Home page");
+  }
+);
 
 app.get("/groceries", (req, res) => {
   res.send(groceryList);
