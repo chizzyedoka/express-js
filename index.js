0afd0b68c66c9d9e21f3e15e89767223eab5adac
CHISOM EDOKA <edokachisom48@gmail.com>
2023-06-30 12:19:31 +0100

    added routes(root,groceries)

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("hi");
+const express = require("express");
+
+const app = express();
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Home page");
+});
+
+app.get("/groceries", (req, res) => {
+  res.send([
+    {
+      item: "milk",
+      quantity: 2,
+    },
+    {
+      item: "cereal",
+      quantity: 1,
+    },
+    {
+      item: "sugar",
+      quantity: 3,
+    },
+  ]);
+});
